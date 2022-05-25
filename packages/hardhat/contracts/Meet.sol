cc881960fce16d90953849a37b0926c54c54e0cc
skyh24 <skyhighfeng@gmail.com>
2022-05-25 21:22:26 +0800

    update contract

--- packages/hardhat/contracts/Meet.sol
+++ packages/hardhat/contracts/Meet.sol
@@ -3,69 +3,143 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-struct Location {
-    address lord;
-    string name;
+struct Land {
+    address host;
+    address []guests;
+    string  name; // name or store
+    string  url;  // your avatar or link
+    LandType typ;
     Position pos;
+    uint   price;
+}
+
+enum LandType {
+    WORK, SHOPING, PARK, ROAD, MEETING, SPORT, OTHER,
+    PLAIN, GRASS, WATER, FOREST, MOUNTAIN, DESERT
 }
 
 struct Position {
-    int32 x; // 111. 111
-    int32 y;
+    int32 lat; // 111. 111
+    int32 lng; 
 }
 
 struct Last {
-    uint256 bNum;
-    uint256 iLoc;
+    uint blkNum;
+    uint locIdx;
 }
 
-contract MeetSH is Ownable {
-    mapping(address => uint256[]) public ownedLocations;
+contract MeetSH is Ownable{
+    mapping(address => uint[]) public ownedLands;
     mapping(address => Last) public lastLight;
-    mapping(int32 => mapping(int32 => uint256)) public hasLocation;
+    
+    mapping(int32 => mapping(int32 => uint)) public posLand; // lat, lng => locIdx
+    Land[] public allLands;
+    uint public landStart;
+    uint public landCount;
 
-    Location[] public locations;
-
-    event LightLand(uint256 index);
+    event LightLand(uint index);
 
     constructor() {
-        locations.push(Location(msg.sender, "Meet", Position(0, 0)));
+        // set 0
+        allLands.push(
+            Land(
+                msg.sender, 
+                new address[](0),
+                "Meet Genesis", 
+                "meet.xyz", 
+                LandType.MEETING, 
+                Position(0, 0), 
+                100 ether
+            ));
     }
 
     // if 8-12, return 5
     // if 13-17, return 10
-    // if 18-22, return 15
+    // if 18-22, return 15 
     function getBound(int32 x) internal returns (int32 lx) {
         int32 z = x % 10;
-        return z < 3 ? x - z - 5 : z > 7 ? x - z + 5 : x - z;
+        return z<3 ? x-z-5 : z>7 ? x-z+5 : x-z;
     }
 
-    function addLocation(Location[] calldata locs) public onlyOwner {
-        for (uint256 i = 0; i < locs.length; i++) {
-            int32 lx = getBound(locs[i].pos.x);
-            int32 ly = getBound(locs[i].pos.y);
-            if (hasLocation[lx][ly] == 0) {
-                locations.push(locs[i]);
-                hasLocation[lx][ly] = locations.length - 1;
+    function addLands(Land[] calldata lands) public onlyOwner {
+        landStart = allLands.length;
+        for (uint i=0; i<lands.length; i++) {
+            int32 lx = getBound(lands[i].pos.lat);
+            int32 ly = getBound(lands[i].pos.lng);
+            if (posLand[lx][ly] == 0) {
+                posLand[lx][ly] = allLands.length;
+                allLands.push(lands[i]);
             }
         }
+        landCount = allLands.length - 1;
     }
 
-    function lightLocation(Position calldata pos) public {
-        int32 lx = getBound(pos.x);
-        int32 ly = getBound(pos.y);
-        uint256 index = hasLocation[lx][ly];
-        require(index != 0, "No land");
+    function changeType(uint index, LandType typ) public onlyOwner {
+        require(landCount >= index, "Index too big!");
+         allLands[index].typ = typ;
+    }
+
+    function withdraw() public onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function mintLand(Position calldata pos, string memory name, string memory url, LandType typ) public payable {
+        require(msg.value >= 1 ether, "Price too low!");
+        int32 lx = getBound(pos.lat);
+        int32 ly = getBound(pos.lng);
+        require(posLand[lx][ly] == 0, "Already has land!"); 
+        posLand[lx][ly] = allLands.length;
+        allLands.push(
+            Land(msg.sender, new address[](0),
+                name, url, typ, pos, 2 ether
+            ));
+        landCount = allLands.length - 1;
+    }
+
+    function buyLand(uint index, string memory name, string memory url) public payable {
+        require(landCount >= index, "Index too big!");
+        require(msg.value >= allLands[index].price, "Price too low!");
+        payable(allLands[index].host).transfer(msg.value * 9 / 10); // transfer
+        allLands[index].host = msg.sender;
+        allLands[index].name = name;
+        allLands[index].url = url;
+        allLands[index].price = allLands[index].price * 2;
+    }
+
+    function modLand(uint index, string memory name, string memory url) public {
+        require(landCount >= index, "Index too big!");
+        require(allLands[index].host == msg.sender, "Only host can modify!");
+        allLands[index].name = name;
+        allLands[index].url = url;
+    }
+
+    function modPrice(uint index, uint price) public {
+        require(landCount >= index, "Index too big!");
+        require(allLands[index].host == msg.sender, "Only host can modify!");
+        allLands[index].price = price;
+    }
+
+
+    function lightLand(Position calldata pos) public {
+        int32 lx = getBound(pos.lat);
+        int32 ly = getBound(pos.lng);
+        uint index = posLand[lx][ly];
+        require(index != 0, "No land"); 
 
         Last memory last = lastLight[msg.sender];
-        Position memory lpos = locations[last.iLoc].pos;
-        int32 dx = pos.x > lpos.x ? pos.x - lpos.x : lpos.x - pos.x;
-        int32 dy = pos.y > lpos.y ? pos.y - lpos.y : lpos.y - pos.y;
-        require(block.number - last.bNum > uint32(dx + dx), "Too short");
+        if (last.blkNum != 0) {
+            Position memory lpos = allLands[last.locIdx].pos;
+            int32 dx = pos.lat > lpos.lat ? pos.lat - lpos.lat : lpos.lat - pos.lat;
+            int32 dy = pos.lng > lpos.lng ? pos.lng - lpos.lng : lpos.lng - pos.lng;
+            require(block.number - last.blkNum > uint32(dx + dx), "Too short");
+        }
 
-        ownedLocations[msg.sender].push(index);
-        Location memory loc = locations[index];
-        loc.lord = msg.sender;
+        ownedLands[msg.sender].push(index);
+        allLands[index].guests.push(msg.sender);
+        lastLight[msg.sender] = Last(block.number, index);
         emit LightLand(index);
+    }
+
+    receive() external payable{
     }
 }
